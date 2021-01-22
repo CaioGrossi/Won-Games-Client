786a1be45061ad7cc37a5e55998777ffe2649317
CaioGrossi <caiogrossi6@gmail.com>
2021-01-22 11:23:47 -0300

    Added empty option for CartList component

--- src/components/CartDropdown/index.tsx
+++ src/components/CartDropdown/index.tsx
@@ -5,13 +5,13 @@ import CartList from 'components/CartList';
 import * as S from './styles';
 
 export type CartDropdownProps = {
-  items: GameItemProps[];
-  total: string;
+  items?: GameItemProps[];
+  total?: string;
 };
 
 const CartDropdown = ({ items, total }: CartDropdownProps) => (
   <S.Wrapper>
-    <Dropdown title={<CartIcon quantity={items.length} />}>
+    <Dropdown title={<CartIcon quantity={items?.length} />}>
       <CartList total={total} items={items} hasButton />
     </Dropdown>
   </S.Wrapper>
