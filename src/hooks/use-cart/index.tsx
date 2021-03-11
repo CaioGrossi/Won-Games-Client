ef2d1d448f577b06aa7ce088280841b692689584
CaioGrossi <caiogrossi6@gmail.com>
2021-03-11 15:31:24 -0300

    Added cart context with initial data

---
+++ src/hooks/use-cart/index.tsx
@@ -0,0 +1,75 @@
+import { useQueryGames } from 'graphql/queries/games';
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { cartMapper } from 'utils/mappers';
+import { getStorageItem } from 'utils/localStorage';
+import formatPrice from 'utils/format-price';
+
+const CART_KEY = 'cartItems';
+
+type CartItem = {
+  id: string;
+  img: string;
+  title: string;
+  price: string;
+};
+
+export type CartContextData = {
+  items: CartItem[];
+  quantity: number;
+  total: string;
+};
+
+export const CartContextDefaultValues = {
+  items: [],
+  quantity: 0,
+  total: '$0.00'
+};
+
+export const CartContext = createContext<CartContextData>(
+  CartContextDefaultValues
+);
+
+export type CartProviderProps = {
+  children: React.ReactNode;
+};
+
+const CartProvider = ({ children }: CartProviderProps) => {
+  const [cartItems, setCartItems] = useState<string[]>([]);
+
+  useEffect(() => {
+    const data = getStorageItem(CART_KEY);
+
+    if (data) {
+      setCartItems(data);
+    }
+  }, []);
+
+  const { data } = useQueryGames({
+    skip: !cartItems?.length,
+    variables: {
+      where: {
+        id: cartItems
+      }
+    }
+  });
+
+  const total = data?.games.reduce((acc, game) => {
+    return acc + game.price;
+  }, 0);
+
+  return (
+    <CartContext.Provider
+      value={{
+        items: cartMapper(data?.games),
+        quantity: cartItems.length,
+        total: formatPrice(total || 0)
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+const useCart = () => useContext(CartContext);
+
+export { CartProvider, useCart };
