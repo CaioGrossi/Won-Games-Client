797262802244111240f61f66cde6f54f06ea93bc
CaioGrossi <caiogrossi6@gmail.com>
2021-03-12 14:35:07 -0300

    Added remove button to CartList

--- src/components/CartList/styles.ts
+++ src/components/CartList/styles.ts
@@ -48,6 +48,11 @@ export const Loading = styled.div`
   `}
 `;
 
+export const GameList = styled.div`
+  max-height: 40rem;
+  overflow-y: auto;
+`;
+
 export const Footer = styled.div`
   ${({ theme }) => css`
     background: ${tint(0.2, theme.colors.lightGray)};
