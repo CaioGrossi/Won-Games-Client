c66d88e752b40a851ba84e0bc1f58f03b4862f28
CaioGrossi <caiogrossi6@gmail.com>
2021-04-29 15:28:25 -0300

    Added removeFromWishlist function

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ import { ThemeProvider } from 'styled-components';
 import theme from 'styles/theme';
 import { useApollo } from '../utils/apollo';
 import { CartProvider } from 'hooks/use-cart';
+import { WishlistProvider } from 'hooks/use-wishlist';
 
 function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApoloState);
@@ -18,21 +19,23 @@ function App({ Component, pageProps }: AppProps) {
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
           <CartProvider>
-            <Head>
-              <title>Won Games</title>
-              <link rel="shortcut icon" href="/img/icon-512.png" />
-              <link rel="apple-touch-icon" href="/img/icon-512.png" />
-              <link rel="manifest" href="/manifest.json" />
-              <meta name="description" content="Won games game store" />
-            </Head>
-            <GlobalStyles />
-            <NextNProgress
-              color="#F231A5"
-              startPosition={0.3}
-              stopDelayMs={200}
-              height={3}
-            />
-            <Component {...pageProps} />
+            <WishlistProvider>
+              <Head>
+                <title>Won Games</title>
+                <link rel="shortcut icon" href="/img/icon-512.png" />
+                <link rel="apple-touch-icon" href="/img/icon-512.png" />
+                <link rel="manifest" href="/manifest.json" />
+                <meta name="description" content="Won games game store" />
+              </Head>
+              <GlobalStyles />
+              <NextNProgress
+                color="#F231A5"
+                startPosition={0.3}
+                stopDelayMs={200}
+                height={3}
+              />
+              <Component {...pageProps} />
+            </WishlistProvider>
           </CartProvider>
         </ThemeProvider>
       </ApolloProvider>
