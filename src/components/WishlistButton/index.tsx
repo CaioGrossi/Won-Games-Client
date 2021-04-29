8ac5337d90a7d110a68fe140b3f7dfe3856857d1
CaioGrossi <caiogrossi6@gmail.com>
2021-04-29 16:14:29 -0300

    Added WishlistButton component

---
+++ src/components/WishlistButton/index.tsx
@@ -0,0 +1,44 @@
+import Button, { ButtonProps } from 'components/Button';
+import { FavoriteBorder, Favorite } from '@styled-icons/material-outlined';
+import { useWishlist } from 'hooks/use-wishlist';
+import { useSession } from 'next-auth/client';
+
+type WishlistButtonProps = {
+  hasText?: boolean;
+  id: string;
+} & Pick<ButtonProps, 'size'>;
+
+const WishlistButton = ({
+  id,
+  hasText,
+  size = 'small'
+}: WishlistButtonProps) => {
+  const [session] = useSession();
+  const { isInWishlist } = useWishlist();
+
+  const ButtonText = isInWishlist(id)
+    ? 'Remove from wishlist'
+    : 'Add to wishlist';
+
+  if (!session) {
+    return null;
+  }
+
+  return (
+    <Button
+      icon={
+        isInWishlist(id) ? (
+          <Favorite aria-label={ButtonText} />
+        ) : (
+          <FavoriteBorder aria-label={ButtonText} />
+        )
+      }
+      minimal
+      size={size}
+    >
+      {hasText && ButtonText}
+    </Button>
+  );
+};
+
+export default WishlistButton;
