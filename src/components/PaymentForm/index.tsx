e5339b707446269c7d347db1b71cd374bbf072a5
CaioGrossi <caiogrossi6@gmail.com>
2021-05-18 15:53:36 -0300

    Finish stripe integration

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -1,28 +1,136 @@
-import { ShoppingCart } from '@styled-icons/material-outlined';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { ShoppingCart, ErrorOutline } from '@styled-icons/material-outlined';
 
 import Heading from 'components/Heading';
 import Button from 'components/Button';
 
+import { StripeCardElementChangeEvent } from '@stripe/stripe-js';
+import { useEffect, useState } from 'react';
+import { useCart } from 'hooks/use-cart';
+import { createPaymentIntent } from 'utils/stripe/methods';
+import { Session } from 'next-auth/client';
+import { useRouter } from 'next/router';
+import { FormLoading } from 'components/Form';
+
 import * as S from './styles';
 
-const PaymentForm = () => {
+type PaymentFormProps = {
+  session: Session;
+};
+
+const PaymentForm = ({ session }: PaymentFormProps) => {
+  const { items } = useCart();
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const { push } = useRouter();
+
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+  const [clientSecret, setClientSecret] = useState('');
+  const [freeGames, setFreeGames] = useState(false);
+
+  useEffect(() => {
+    async function setPaymentMode() {
+      if (items.length) {
+        const data = await createPaymentIntent({ items, token: session.jwt });
+
+        if (data.freeGames) {
+          setFreeGames(true);
+          return;
+        }
+
+        if (data.error) {
+          setError(data.error);
+          return;
+        }
+        setFreeGames(false);
+        setClientSecret(data.client_secret);
+      }
+    }
+
+    setPaymentMode();
+  }, [items, session]);
+
+  const handleChange = async (event: StripeCardElementChangeEvent) => {
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : '');
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    setLoading(true);
+
+    if (freeGames) {
+      // salva no banco
+      // redireciona para success
+      push('/success');
+      return;
+    }
+
+    const payload = await stripe!.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements!.getElement(CardElement)!
+      }
+    });
+
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`);
+      setLoading(false);
+    } else {
+      setError(null);
+      setLoading(false);
+      push('/success');
+    }
+  };
+
   return (
     <S.Wrapper>
-      <S.Body>
-        <Heading lineBottom color="black" size="small">
-          Payment
-        </Heading>
-      </S.Body>
+      <form onSubmit={handleSubmit}>
+        <S.Body>
+          <Heading lineBottom color="black" size="small">
+            Payment
+          </Heading>
 
-      <S.Footer>
-        <Button as="a" fullWidth minimal>
-          Continue shopping
-        </Button>
+          {freeGames ? (
+            <S.FreeGames>Only free games, click buy and enjoy!</S.FreeGames>
+          ) : (
+            <CardElement
+              options={{
+                hidePostalCode: true,
+                style: {
+                  base: {
+                    fontSize: '16px'
+                  }
+                }
+              }}
+              onChange={handleChange}
+            />
+          )}
 
-        <Button fullWidth icon={<ShoppingCart />}>
-          Buy now
-        </Button>
-      </S.Footer>
+          {error && (
+            <S.Error>
+              <ErrorOutline size={20} />
+              {error}
+            </S.Error>
+          )}
+        </S.Body>
+
+        <S.Footer>
+          <Button as="a" fullWidth minimal>
+            Continue shopping
+          </Button>
+
+          <Button
+            fullWidth
+            icon={loading ? <FormLoading /> : <ShoppingCart />}
+            disabled={!freeGames && (disabled || !!error)}
+          >
+            {!loading && <span>Buy now</span>}
+          </Button>
+        </S.Footer>
+      </form>
     </S.Wrapper>
   );
 };
--- src/templates/Cart/index.tsx
+++ src/templates/Cart/index.tsx
@@ -1,3 +1,6 @@
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
 import { Container } from 'components/Container';
 import { Divider } from 'components/Divider';
 import { GameCardProps } from 'components/GameCard';
@@ -9,14 +12,19 @@ import Showcase from 'components/Showcase';
 import Base from 'templates/Base';
 
 import * as S from './styles';
+import { Session } from 'next-auth/client';
 
 export type CartProps = {
+  session: Session;
   recommendedTitle: string;
   recommendedGames: GameCardProps[];
   recommendedHighlight: HighlightProps;
 } & CartListProps;
 
+const stripe = loadStripe(`${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}`);
+
 const Cart = ({
+  session,
   recommendedTitle,
   recommendedGames,
   recommendedHighlight
@@ -31,7 +39,9 @@ const Cart = ({
         <S.Content>
           <CartList />
 
-          <PaymentForm />
+          <Elements stripe={stripe}>
+            <PaymentForm session={session} />
+          </Elements>
         </S.Content>
 
         <Divider />
